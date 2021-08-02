a009a1b390d7d6ce1b31a0dc30b91458c703c23f
Dipali Vaidya <dipali.vaidya@datafortune.com>
2021-08-02 17:34:04 +0530

    updated resource screen page

--- src/main/java/com/PRMSTool/Pages/ResourceScreenPage.java
+++ src/main/java/com/PRMSTool/Pages/ResourceScreenPage.java
@@ -221,7 +221,7 @@ public class ResourceScreenPage extends TestBase {
 		resourceName.sendKeys("TestResource");
 		
 		employeeID.clear();
-		employeeID.sendKeys("DS448");
+		employeeID.sendKeys("DS444");
 		//Thread.sleep(5000);
 		
 		/*
