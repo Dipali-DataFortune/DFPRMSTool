a20898ac9895f2036ba2b6484ed577b31e469b6e
Dipali Vaidya <dipali.vaidya@datafortune.com>
2021-08-03 17:37:08 +0530

    updated test base

--- src/main/java/com/PRMSTool/base/TestBase.java
+++ src/main/java/com/PRMSTool/base/TestBase.java
@@ -49,8 +49,10 @@ public class TestBase {
 
 			// System.setProperty("webdriver.chrome.driver", currentDirectory +
 			// "\\Driver\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dipali.vaidya\\Documents\\ChromeDriver\\chromedriver.exe");
+			
 			System.setProperty("webdriver.chrome.driver",
-					"C:\\Users\\Dipali.vaidya\\Documents\\ChromeDriver\\chromedriver.exe");
+					"C:\\Users\\Dipali.vaidya\\Documents\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
 
 			driver = new ChromeDriver();
 			driver.get(prop.getProperty("url"));
